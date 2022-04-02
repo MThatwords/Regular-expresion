d3f3c5d4b9d0b4c9e47ad9f3016e3be9f493f105
MThanCode <ricardobvil116@gmail.com>
2022-04-01 22:01:26 -0600

    llego hasta case ignore

---
+++ resularExpresions.js
@@ -0,0 +1,9 @@
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // todo pegado
+let result = petRegex.test(petString);// true , si esta dog o cat ...
+
+let fccRegex = /freecodecamp/ig; // i: ignore g:global
+
+
+
+
