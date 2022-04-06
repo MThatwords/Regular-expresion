809050e94759e55838f7a917cb98b141edc3d2bd
MThanCode <ricardobvil116@gmail.com>
2022-04-05 22:38:43 -0600

    nuevas funciones

--- resularExpresions.js
+++ resularExpresions.js
@@ -7,3 +7,37 @@ let fccRegex = /freecodecamp/ig; // i: ignore g:global
 
 
 
+function long(s){
+  return Math.max(...s.split(" ").map(word => word.length))
+}
+
+console.log(long("The quick brown fox jumped over the lazy dog"));
+
+
+function largestOfFour(arr) {
+  return arr.map(function(group){
+    return group.reduce(function(prev, curr){
+      return curr> prev ? curr:prev;
+        
+      })
+    });
+}
+
+function confirmEnding1(str, target) {
+  return  str.endsWith(target)
+  
+}
+function confirmEnding2(str, target) {
+  return  str.slice(str.length - target.length) === target
+  
+}
+function confirmEnding3(str, target) {
+  let re = new RegExp(target + "$","i")
+  return  re.test(str)
+  
+}
+function confirmEnding4(str, target) {
+  return  str.slice(- target.length) === target
+  
+}
+
